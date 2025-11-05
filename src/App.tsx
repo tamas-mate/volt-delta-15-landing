d9bed04bc865bf97e72933973d2cffe65d8ff2b8
Tamas Mate <tamasmate.dev@gmail.com>
2025-11-05 13:17:37 +0200

    Implemented the Showcase component

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import { ScrollTrigger } from "gsap/all";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import ProductViewer from "./components/ProductViewer";
+import Showcase from "./components/Showcase";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
-    <main className="flex flex-col items-center gap-y-50">
+    <div className="flex min-h-screen flex-col gap-y-15">
       <Navbar />
-      <Hero />
-      <ProductViewer />
-    </main>
+      <main className="flex flex-col gap-y-15">
+        <Hero />
+        <ProductViewer />
+        <Showcase />
+      </main>
+    </div>
   );
 };
 
