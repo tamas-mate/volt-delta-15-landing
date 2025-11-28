a2d44fc47291815ac3d24bd481fd0f9432adfc81
Tamas Mate <tamasmate.dev@gmail.com>
2025-11-28 17:12:37 +0200

    Config and package updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,29 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+  plugins: [
+    tailwindcss(),
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom", "react-responsive"],
+          "three-stack": [
+            "three",
+            "@react-three/fiber",
+            "@react-three/drei",
+            "three-stdlib",
+          ],
+          "gsap-stack": ["gsap", "@gsap/react"],
+          "state-vendor": ["zustand"],
+        },
+      },
+    },
+  },
 });
